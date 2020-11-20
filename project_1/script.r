113688c76c9c3b8395d0d76b2da55777141b727d
Rambod Rahmani <rambodrahmani@autistici.org>
2020-11-20 15:29:41 +0100

    Implement alternative exponential regression model.

--- project_1/script.r
+++ project_1/script.r
@@ -5,6 +5,10 @@ rm(list = ls())
 # importazione librerie da utilizzare
 library(corrplot)
 
+################################################################################
+####################    IMPORTAZIONE E PULIZIA DEI DATI    #####################
+################################################################################
+
 # caricamento dei dati dal file csv
 data = read.csv('data.csv', header = TRUE, sep = ";")
 
@@ -78,6 +82,10 @@ head(data)
 plot(data)
 corrplot(cor(data), method = "number")
 
+################################################################################
+####################          REGRESSIONE LINEARE           ####################
+################################################################################
+
 # costruzione modello di regressione lineare
 r = matrix(ncol = 2, nrow = 4)
 
@@ -108,7 +116,7 @@ axis(1 , at = 0:4, labels = 0:4)
 lines(r[,2], pch = 19, type = "b", col = "blue")
 legend(3.53, 0.9696, legend=c(r_squared, r_squared_adj), col=c("red", "blue"), lty=1:1)
 
-# modello di regressione lineare
+# modello di regressione lineare scelto
 lm = lm.3
 summary(lm)
 
@@ -170,7 +178,8 @@ shapiro.test(lm.resid)
 # deviazione dal un modello Gaussiano
 lm.resid.ord = sort(lm.resid)
 lm.resid<-lm.resid[-which(lm.resid %in% lm.resid.ord[1:10])]
-lm.resid<-lm.resid[-which(lm.resid %in% lm.resid.ord[400:467])]
+lm.resid<-lm.resid[-which(lm.resid %in% lm.resid.ord[360])]
+lm.resid<-lm.resid[-which(lm.resid %in% lm.resid.ord[length(lm.resid.ord)])]
 boxplot(lm.resid, main="Boxplot residui dopo la rimozione degli outliers", col=(c("gold","darkgreen")), outcol="red")
 
 # plot residui: non ho usato predict(lm) dato che avendo eliminato alcuni dei
@@ -191,13 +200,123 @@ kurtosi = mean(((lm.resid - mean(lm.resid)) / sd(lm.resid))^4) - 3
 kurtosi
 shapiro.test(lm.resid)
 
-# costruzione modello regressione esponenziale
-lm.2<-lm(log(Rmax)~log(TotalCores)+log(Rpeak)+log(ProcessorSpeed)+log(CoresPerSocket), data = data)
+################################################################################
+####################        REGRESSIONE ESPONENZIALE        ####################
+################################################################################
+
+# costruzione modello di regressione esponenziale
+r = matrix(ncol = 2, nrow = 4)
+
+lm.1<-lm(log(Rmax)~log(Rpeak)+log(TotalCores)+log(CoresPerSocket)+log(ProcessorSpeed), data = data)
+summary(lm.1)
+r[1,]= c(summary(lm.1)$r.squared, summary(lm.1)$adj.r.squared)
+
+lm.2<-lm(log(Rmax)~log(Rpeak)+log(TotalCores)+log(CoresPerSocket), data = data)
 summary(lm.2)
-lm.2.resid = residuals(lm.2)
-plot(predict(lm.2), lm.2.resid)
-hist(lm.2.resid, 100, freq = FALSE)
-lines(sort(lm.2.resid), dnorm(sort(lm.2.resid), mean(lm.2.resid), sd(lm.2.resid)), col="red")
-qqnorm(lm.2.resid)
-qqline(lm.2.resid, col="red")
-shapiro.test(lm.2.resid)
+r[2,]= c(summary(lm.2)$r.squared, summary(lm.2)$adj.r.squared)
+
+lm.3<-lm(log(Rmax)~log(Rpeak)+log(TotalCores), data = data)
+summary(lm.3)
+r[3,]= c(summary(lm.3)$r.squared, summary(lm.3)$adj.r.squared)
+
+lm.4<-lm(log(Rmax)~log(Rpeak), data = data)
+summary(lm.4)
+r[4,]= c(summary(lm.4)$r.squared, summary(lm.4)$adj.r.squared)
+
+# traccio il grafico di comparazione degli R^2 e R^2 corretti
+ymin = min(r)
+ymax = max(r)
+r_squared <- expression(R^2)
+r_squared_adj <- expression(R^2 ~ Corretto)
+xl <- expression(R^2 ~ e ~ R^2 ~ Corretto ~ dei ~ 4 ~ Modelli ~ di ~ Regressione ~ Esponenziale ~ Valutati)
+plot(r[,1], pch = 19, type = "b", col = "red", xaxt="n", ylab = "", ylim = c(ymin,ymax), xlab = xl, cex = 1.5, cex.axis = 1.5, cex.lab = 1.5)
+axis(1 , at = 0:4, labels = 0:4)
+lines(r[,2], pch = 19, type = "b", col = "blue")
+legend(3.53, 0.8496, legend=c(r_squared, r_squared_adj), col=c("red", "blue"), lty=1:1)
+
+# modello di regressione esponenziale scelto
+lm = lm.3
+summary(lm)
+
+# analisi dei residui del modello di regressione esponenziale
+lm.resid = residuals(lm)
+plot(predict(lm), lm.resid)
+par(mfrow=c(1, 2))
+hist(lm.resid, 100, freq = FALSE)
+lines(sort(lm.resid), dnorm(sort(lm.resid), mean(lm.resid), sd(lm.resid)), col="red", lwd=2)
+qqnorm(lm.resid)
+qqline(lm.resid, col="red", lwd=2)
+skewness = mean(((lm.resid - mean(lm.resid)) / sd(lm.resid))^3)
+skewness
+kurtosi = mean(((lm.resid - mean(lm.resid)) / sd(lm.resid))^4) - 3
+kurtosi
+shapiro.test(lm.resid)
+
+# approfondimento analisi dei residui: calcolo della correlazione tra i
+# fattori di ingresso e tra i fattori di ingresso e i residui
+cor(data$Rpeak, data$TotalCores)
+cor(data$Rpeak, lm.resid)
+cor(data$TotalCores, lm.resid)
+
+# approfondimento analisi dei residui: valutazione modello di regressione
+# lineare con tutti i fattori
+lm.1.resid = residuals(lm.1)
+shapiro.test(lm.1.resid)
+
+# approfondimento analisi dei residui: valutazione modello di regressione
+# lineare semplice
+lm.4.resid = residuals(lm.4)
+shapiro.test(lm.4.resid)
+
+# approfondimento analisi dei residui: rimozione residui relativi agli outliers
+boxplot(lm.resid, main="Boxplot residui iniziali", col=(c("gold","darkgreen")), outcol="red")
+Residuals_outliers <- boxplot(lm.resid, plot=FALSE)$out
+Residuals_outliers <- rev(sort(Residuals_outliers))
+Residuals_outliers
+lm.resid<-lm.resid[-which(lm.resid %in% Residuals_outliers[1:length(Residuals_outliers)])]
+
+# plot residui: non ho usato predict(lm) dato che avendo eliminato alcuni dei
+# residui ora ho dimensioni differenti
+plot(lm.resid)
+
+# visualizziamo nuovamente i grafici relativi ai residui per vedere se la loro
+# distribuzione sia migliorata o meno
+par(mfrow=c(1, 2))
+hist(lm.resid, 100, freq = FALSE)
+lines(sort(lm.resid), dnorm(sort(lm.resid), mean(lm.resid), sd(lm.resid)), col="red", lwd=2)
+qqnorm(lm.resid)
+qqline(lm.resid, col="red", lwd=2)
+skewness = mean(((lm.resid - mean(lm.resid)) / sd(lm.resid))^3)
+skewness
+kurtosi = mean(((lm.resid - mean(lm.resid)) / sd(lm.resid))^4) - 3
+kurtosi
+shapiro.test(lm.resid)
+
+# approfondimento analisi dei residui: rimozione dei residui che causano la
+# deviazione dal un modello Gaussiano
+lm.resid.ord = sort(lm.resid)
+lm.resid<-lm.resid[-which(lm.resid %in% lm.resid.ord[1:11])]
+lm.resid<-lm.resid[-which(lm.resid %in% lm.resid.ord[279:279])]
+boxplot(lm.resid, main="Boxplot residui dopo la rimozione degli outliers", col=(c("gold","darkgreen")), outcol="red")
+
+# plot residui: non ho usato predict(lm) dato che avendo eliminato alcuni dei
+# residui ora ho dimensioni differenti
+plot(lm.resid)
+
+# visualizziamo nuovamente i grafici relativi ai residui per vedere se la loro
+# distribuzione sia migliorata o meno
+par(mfrow=c(1, 2))
+hist(lm.resid, 100, freq = FALSE)
+lines(sort(lm.resid), dnorm(sort(lm.resid), mean(lm.resid), sd(lm.resid)), col="red", lwd=2)
+lines(density(lm.resid), col="blue", lwd=2)
+qqnorm(lm.resid)
+qqline(lm.resid, col="red", lwd=2)
+skewness = mean(((lm.resid - mean(lm.resid)) / sd(lm.resid))^3)
+skewness
+kurtosi = mean(((lm.resid - mean(lm.resid)) / sd(lm.resid))^4) - 3
+kurtosi
+shapiro.test(lm.resid)
+
+################################################################################
+####################               PREDIZIONE               ####################
+################################################################################
