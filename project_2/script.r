b89fc3056dff38a9146e2a71aeb11ffc7cd8400e
Rambod Rahmani <rambodrahmani@autistici.org>
2020-12-11 23:44:48 +0100

    Improved the classification model using LDA, QDA and logistic regression.

--- project_2/script.r
+++ project_2/script.r
@@ -78,7 +78,7 @@ with(data, table(Segment))
 data$Site <- as.numeric(data$Site)
 data$Manufacturer <- as.numeric(data$Manufacturer)
 data$Country <- as.numeric(data$Country)
-data$Segment <- as.numeric(data$Segment)
+#data$Segment <- as.numeric(data$Segment)
 data$Architecture <- as.numeric(data$Segment)
 data$Processor <- as.numeric(data$Processor)
 data$Processor.Technology <- as.numeric(data$Processor.Technology)
@@ -94,24 +94,29 @@ data$Continent <- as.numeric(data$Continent)
 # eliminiamo eventuali NA, NaN generati durante la conversione dal tipo Factor
 # al tipo numeric
 sum(is.na(data))
-na.omit(data)
+data<-na.omit(data)
 
 # controlliamo la nuova struttura dei dati
 str(data)
 
 ################################################################################
+#####################                  PCA                 #####################
+################################################################################
+pca = princomp(scale(subset(data, select=-Segment)))
+summary(pca)
+biplot(pca)
+loadings(pca)
+
+# costruiamo il data frame con le osservazioni secondo la PCA
+data.pca<-as.data.frame(pca$scores[,1:10])
+data.pca<-cbind(data.pca, data$Segment)
+colnames(data.pca)[11] <- "Segment"
+
+################################################################################
 ####################     ANALISI DISCRIMINANTE LINEARE     #####################
 ################################################################################
-lda = lda(Segment~., data=data)
-plot(lda, col = 1 + as.numeric(data$Segment))
-
-# data$Nmax variables appear to be costant within groups
-# rimozione della colonna data$Nmax, che fornisce valori costanti per gruppi
-# differenti
-data$Nmax <- NULL
-
-lda = lda(Segment~., data=data)
-plot(lda, col = 1 + as.numeric(data$Segment))
+lda = lda(Segment~., data=data.pca)
+plot(lda, col = 1 + as.numeric(data.pca$Segment))
 
 lda.values=predict(lda)
 plot(lda.values$x, pch=20, col = as.numeric(data$Segment)+1)
@@ -135,35 +140,42 @@ sum(data$Segment != lda.values$class)/length(data$Segment)
 # stato da preoccuparsi fosse stato il contrario
 
 # Autovaluazione Analisi Discriminante Lineare
-
+acc = rep(0, 30)
+l = nrow(data)
+for(i in 1:30) {
+  idx = sample(l, 30)
+  train = data.pca[-idx,]
+  test = data.pca[idx,]
+  train.lda = lda(Segment~., data=train)
+  test.pt = predict(train.lda, newdata=test)$class
+  acc[i] = sum(test.pt == data$Segment[idx])/30
+}
+mean(acc)
+sd(acc)
+hist(acc)
 
 ################################################################################
 ####################   ANALISI DISCRIMINANTE QUADRATICA    #####################
 ################################################################################
-qda = qda(Segment~., data=data, CV=F)
-plot(qda, col = 1 + as.numeric(data$Segment))
 
-# data$Nmax variables appear to be costant within groups
-# rimozione della colonna data$Nmax, che fornisce valori costanti per gruppi
-# differenti
-data$Nmax <- NULL
+# costruiamo il data frame con le osservazioni secondo la PCA
+data.pca<-as.data.frame(pca$scores[,1:7])
+data.pca<-cbind(data.pca, data$Segment)
+colnames(data.pca)[8] <- "Segment"
 
-qda = qda(Segment~., data=data)
-plot(qda, col = 1 + as.numeric(data$Segment))
-
-lda.values=predict(lda)
-plot(lda.values$x, pch=20, col = as.numeric(data$Segment)+1)
+# modello per analisi discriminante quadratica
+qda = qda(Segment~., data=data.pca)
 
 # valutiamo l’accuratezza
-sum(data$Segment == lda.values$class)
-sum(data$Segment == lda.values$class)/length(data$Segment)
+sum(data$Segment == predict(qda)$class)
+sum(data$Segment == predict(qda)$class)/length(data$Segment)
 
 # vengono classificati correttamente (nel segment di mercato di appartenenza)
 # 498 supercomputer su 500, accuratezza del 99.6%
 
 # valutiamo l’errore
-sum(data$Segment != lda.values$class)
-sum(data$Segment != lda.values$class)/length(data$Segment)
+sum(data$Segment != predict(qda)$class)
+sum(data$Segment != predict(qda)$class)/length(data$Segment)
 
 # vengono classificati erroneamente solamente 2 supercomputer su 500, errore del
 # 0.4%
@@ -173,3 +185,98 @@ sum(data$Segment != lda.values$class)/length(data$Segment)
 # stato da preoccuparsi fosse stato il contrario
 
 # Autovaluazione Analisi Discriminante Lineare
+acc = rep(0, 1)
+l = nrow(data)
+for(i in 1:30) {
+  idx = sample(l, 1)
+  train = data.pca[-idx,]
+  test = data.pca[idx,]
+  train.qda = qda(Segment~., data=train)
+  test.pt = predict(train.qda, newdata=test)$class
+  acc[i] = sum(test.pt == data$Segment[idx])/1
+}
+mean(acc)
+sd(acc)
+hist(acc)
+
+################################################################################
+#####################         REGRESSIONE LOGISTICA        #####################
+################################################################################
+
+# costruiamo il data frame con le osservazioni secondo la PCA
+data.pca<-as.data.frame(pca$scores[,1:9])
+data.pca<-cbind(data.pca, data$Segment)
+colnames(data.pca)[10] <- "Segment"
+
+# regressione logistica per classificare Academic
+dt = data.pca[,1:9]
+dt$class<-as.numeric(data.pca$Segment == "Academic")
+academic.glm = glm(class~., family = binomial, data = dt)
+academic.p = predict(academic.glm, type="response")
+
+# costruiamo il data frame con le osservazioni secondo la PCA
+data.pca<-as.data.frame(pca$scores[,1:12])
+data.pca<-cbind(data.pca, data$Segment)
+colnames(data.pca)[13] <- "Segment"
+
+# regressione logistica per classificare Government
+dt = data.pca[,1:12]
+dt$class<-as.numeric(data.pca$Segment == "Government")
+government.glm = glm(class~., family = binomial, data = dt)
+government.p = predict(government.glm, type="response")
+
+# costruiamo il data frame con le osservazioni secondo la PCA
+data.pca<-as.data.frame(pca$scores[,1:12])
+data.pca<-cbind(data.pca, data$Segment)
+colnames(data.pca)[13] <- "Segment"
+
+# regressione logistica per classificare Industry
+dt = data.pca[,1:12]
+dt$class<-as.numeric(data.pca$Segment == "Industry")
+industry.glm = glm(class~., family = binomial, data = dt)
+industry.p = predict(industry.glm, type="response")
+
+# costruiamo il data frame con le osservazioni secondo la PCA
+data.pca<-as.data.frame(pca$scores[,1:12])
+data.pca<-cbind(data.pca, data$Segment)
+colnames(data.pca)[13] <- "Segment"
+
+# regressione logistica per classificare Others
+dt = data.pca[,1:12]
+dt$class<-as.numeric(data.pca$Segment == "Others")
+others.glm = glm(class~., family = binomial, data = dt)
+others.p = predict(others.glm, type="response")
+
+# costruiamo il data frame con le osservazioni secondo la PCA
+data.pca<-as.data.frame(pca$scores[,1:12])
+data.pca<-cbind(data.pca, data$Segment)
+colnames(data.pca)[13] <- "Segment"
+
+# regressione logistica per classificare Research
+dt = data.pca[,1:12]
+dt$class<-as.numeric(data.pca$Segment == "Research")
+research.glm = glm(class~., family = binomial, data = dt)
+research.p = predict(research.glm, type="response")
+
+# costruiamo il data frame con le osservazioni secondo la PCA
+data.pca<-as.data.frame(pca$scores[,1:9])
+data.pca<-cbind(data.pca, data$Segment)
+colnames(data.pca)[10] <- "Segment"
+
+# regressione logistica per classificare Vendor
+dt = data.pca[,1:9]
+dt$class<-as.numeric(data.pca$Segment == "Vendor")
+vendor.glm = glm(class~., family = binomial, data = dt)
+vendor.p = predict(vendor.glm, type="response")
+
+# Calcoliamo la risposta finale e l’accuratezza.
+probs = cbind(academic.p, government.p, industry.p, others.p, research.p, vendor.p)
+l = length(data.pca$Segment)
+res = rep(0, l)
+for(i in 1:l) {
+  res[i] = which.max(probs[i,])
+}
+res <- factor(res)
+levels(res) <- c("Academic", "Government", "Industry", "Others", "Research", "Vendor")
+sum(data.pca$Segment == res)
+sum(data.pca$Segment == res)/l
